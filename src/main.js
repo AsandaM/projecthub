2628b4a94fd0941181b8aa606a33717337198fd5
divineuloko <145670721+divineuloko@users.noreply.github.com>
2025-06-23 14:52:39 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,29 +6,6 @@ import 'bootstrap';
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import router from './router'
-import { Amplify } from 'aws-amplify'
-
-Amplify.configure({
-  Auth: {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_JHvEIPcFi',
-    userPoolWebClientId: '5a1ubeeavekrg8ejp4oqsap437',
-    mandatorySignIn: true,
-  },
-  API: {
-    endpoints: [
-      {
-        name: 'createProject',
-        endpoint: 'https://7f7w0zcocc.execute-api.us-east-1.amazonaws.com/create2',
-        region: 'us-east-1'
-      }
-    ]
-  }
-});
-
-
-
 const app = createApp(App)
 
 app.use(router)
