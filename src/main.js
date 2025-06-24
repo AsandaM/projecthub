2c3e3092147f11a902ca083b77e7a80f532d7cb4
divineuloko <145670721+divineuloko@users.noreply.github.com>
2025-06-24 16:53:23 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap'; 
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,13 +9,5 @@ import router from './router'
 
 const app = createApp(App)
 
-// Add Vue configuration
-app.config.unwrapInjectedRef = true
-app.config.compilerOptions = {
-  isCustomElement: tag => tag.includes('-')
-}
-
 app.use(router)
 app.mount('#app')
-
-window.bootstrap = bootstrap
